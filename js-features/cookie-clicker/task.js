b96de1add5c3e9a7f2e5655d8899f20e6c629c9e
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-09-21 17:50:10 +0300

    first commit cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,24 +1,23 @@
+const cookie = document.querySelector('.clicker__cookie');
+const counter = document.getElementById('clicker__counter');
 
-        const cookie = document.getElementById('cookie');
-        const counter = document.getElementById('counter');
+let clickCount = 0;
+let isCookieLarge = true; 
 
-        let clickCount = 0;
-        let isCookieLarge = true; 
+cookie.onclick = () => {
+    clickCount++;
 
-        cookie.onclick = () => {
-            clickCount++;
+    // Обновляем счётчик
+    counter.textContent = clickCount;
 
-            // Обновляем счётчик
-            counter.textContent = clickCount;
+    // Чередуем уменьшение и увеличение размера печеньки
+    if (isCookieLarge) {
+        cookie.setAttribute('width', '80');
+        cookie.setAttribute('height', '80');
+    } else {
+        cookie.setAttribute('width', '100');
+        cookie.setAttribute('height', '100');
+    }
 
-            // Чередуем уменьшение и увеличение размера печеньки
-            if (isCookieLarge) {
-                cookie.setAttribute('width', '80');
-                cookie.setAttribute('height', '80');
-            } else {
-                cookie.setAttribute('width', '100');
-                cookie.setAttribute('height', '100');
-            }
-
-            isCookieLarge = !isCookieLarge;
-        };
+    isCookieLarge = !isCookieLarge;
+};
