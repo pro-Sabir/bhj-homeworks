c5039e8c056f219f2301830296d3389e66752661
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-09-28 22:06:53 +0300

    first commit tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,36 @@
+// Находим все элементы с классом has-tooltip
+const tooltipTriggers = document.querySelectorAll('.has-tooltip');
+
+// Создаем функцию для отображения подсказки
+function showTooltip(event) {
+  event.preventDefault(); // Отменяем стандартное действие ссылки
+  const tooltipText = this.getAttribute('title'); // Получаем текст подсказки из атрибута title
+  const tooltip = document.querySelector('.tooltip'); // Находим элемент подсказки
+
+  // Устанавливаем текст подсказки
+  tooltip.textContent = tooltipText;
+
+  // Показываем подсказку
+  tooltip.classList.add('tooltip_active');
+
+  // Устанавливаем позицию подсказки (над элементом)
+  const rect = this.getBoundingClientRect();
+  tooltip.style.top = rect.top - tooltip.offsetHeight - 10 + 'px';
+  tooltip.style.left = rect.left + (rect.width - tooltip.offsetWidth) / 2 + 'px';
+}
+
+// Добавляем обработчик клика на каждый элемент с классом has-tooltip
+tooltipTriggers.forEach((trigger) => {
+  trigger.addEventListener('click', showTooltip);
+});
+
+// Добавляем обработчик клика на документе для закрытия подсказки
+document.addEventListener('click', function closeTooltip() {
+  const tooltip = document.querySelector('.tooltip'); // Находим элемент подсказки
+  tooltip.classList.remove('tooltip_active'); // Скрываем подсказку
+});
+
+// Останавливаем всплытие события клика на элементе подсказки
+document.querySelector('.tooltip').addEventListener('click', function (event) {
+  event.stopPropagation();
+});
