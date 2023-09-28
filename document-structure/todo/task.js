a215529ff3901691205b258f5e64012a0d982c64
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-09-28 22:10:27 +0300

    first commit todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,52 @@
+// Находим нужные элементы в DOM
+const inputTask = document.querySelector('.tasks__input');
+const addButton = document.querySelector('.tasks__add');
+const taskList = document.querySelector('.tasks__list');
+
+// Функция для добавления задачи
+function addTask() {
+  // Получаем текст из поля ввода
+  const taskText = inputTask.value.trim();
+
+  // Проверяем, что текст не пустой
+  if (taskText !== '') {
+    // Создаем новый элемент задачи
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('task');
+
+    // Создаем элемент для заголовка задачи
+    const taskTitle = document.createElement('div');
+    taskTitle.classList.add('task__title');
+    taskTitle.textContent = taskText;
+
+    // Создаем элемент для кнопки удаления задачи
+    const removeButton = document.createElement('a');
+    removeButton.classList.add('task__remove');
+    removeButton.innerHTML = '&times;';
+
+    // Добавляем обработчик события для кнопки удаления
+    removeButton.addEventListener('click', function () {
+      taskList.removeChild(taskItem);
+    });
+
+    // Добавляем заголовок и кнопку удаления к элементу задачи
+    taskItem.appendChild(taskTitle);
+    taskItem.appendChild(removeButton);
+
+    // Добавляем элемент задачи к списку задач
+    taskList.appendChild(taskItem);
+
+    // Очищаем поле ввода
+    inputTask.value = '';
+  }
+}
+
+// Добавляем обработчик события для кнопки "Добавить задачу"
+addButton.addEventListener('click', addTask);
+
+// Добавляем обработчик события для поля ввода при нажатии Enter
+inputTask.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+});
