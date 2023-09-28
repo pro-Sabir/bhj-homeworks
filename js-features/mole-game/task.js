d2f1a20599c27f6928b7392ad42d773301af95d0
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-09-28 21:48:50 +0300

    first commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,78 +1,70 @@
-const deadDisplay = document.getElementById('dead');
-const lostDisplay = document.getElementById('lost');
-const holes = document.querySelectorAll('.hole');
 
-let score = 0;
-let attempts = 5;
-let moleInterval;
+function getHole(index) {
+    return document.getElementById(`hole${index}`);
+  }
+  
 
-function getRandomTime(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
+  let deadCounter = 0;
+  let lostCounter = 0;
+  
 
-function getRandomHole(holes) {
-    const index = Math.floor(Math.random() * holes.length);
-    const hole = holes[index];
-    return hole;
-}
+  function onHoleClick(event) {
+    const hole = event.target;
+    
+   
+    if (hole.classList.contains('hole_has-mole')) {
 
-function peep() {
-    const time = getRandomTime(200, 1000);
-    const hole = getRandomHole(holes);
-    hole.classList.add('up');
-    setTimeout(() => {
-        hole.classList.remove('up');
-        if (!hole.classList.contains('whacked')) {
-            attempts--;
-            lostDisplay.textContent = attempts;
-            if (attempts === 0) {
-                alert('Игра окончена. Вы проиграли.');
-                resetGame();
-            }
-        }
+      deadCounter++;
+      document.getElementById('dead').textContent = deadCounter;
+    } else {
 
-        updateScore();
-    }, time);
-}
-
-function updateScore() {
-    score++;
-    deadDisplay.textContent = score;
-    if (score === 10) {
-        alert('Победа! Вы убили 10 кротов!');
-        resetGame();
+      lostCounter++;
+      document.getElementById('lost').textContent = lostCounter;
     }
-}
+  
 
-function startGame() {
-    score = 0;
-    attempts = 5;
-    lostDisplay.textContent = attempts;
-    deadDisplay.textContent = score;
-    moleInterval = setInterval(peep, 1000);
-}
+    if (deadCounter === 10) {
+      alert('Вы выиграли! Поздравляем!');
+      resetGame();
+    } else if (lostCounter === 5) {
+      alert('Игра окончена. Вы проиграли.');
+      resetGame();
+    }
+  }
+  
 
-function resetGame() {
-    clearInterval(moleInterval);
-    attempts = 5;
-    lostDisplay.textContent = attempts;
-    score = 0;
-    deadDisplay.textContent = score;
-}
+  function resetGame() {
+    deadCounter = 0;
+    lostCounter = 0;
+    document.getElementById('dead').textContent = deadCounter;
+    document.getElementById('lost').textContent = lostCounter;
+  
+  
+    for (let i = 1; i <= 9; i++) {
+      const hole = getHole(i);
+      hole.classList.remove('hole_has-mole');
+    }
+    
+  
+    startNewGame();
+  }
+  
+ 
+  function startNewGame() {
+  
+    const randomHole = Math.floor(Math.random() * 9) + 1;
+    
 
-holes.forEach(hole => {
-    hole.addEventListener('click', () => {
-        if (hole.classList.contains('up')) {
-            hole.classList.remove('up');
-            if (!hole.classList.contains('whacked')) {
-                hole.classList.add('whacked');
-                updateScore();
-            }
-        }
-    });
-});
+    const hole = getHole(randomHole);
+    hole.classList.add('hole_has-mole');
+  }
+  
 
-
-document.getElementById('start-button').addEventListener('click', () => {
-    startGame();
-});
+  for (let i = 1; i <= 9; i++) {
+    const hole = getHole(i);
+    hole.addEventListener('click', onHoleClick);
+  }
+  
+  
+  startNewGame();
+  
